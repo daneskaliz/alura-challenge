02b50db1f7df4f6381d4d916b6463cee6e4edc19
Daneska Liz <daneskaliz@gmail.com>
2024-05-29 07:28:36 -0400

    Agregar validación de formulario de contacto.

--- validacion.js
+++ validacion.js
@@ -1,1 +1,30 @@
-//Haz tú validación en javascript acá
+const contactForm = document.getElementById("contacto");
+let nameField = document.getElementById("nombre");
+let emailField = document.getElementById("email");
+let subjectField = document.getElementById("asunto");
+let messageField = document.getElementById("mensagem");
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const someFieldIsEmpty = validateForm();
+    if (someFieldIsEmpty) return alert("Por favor completa todos los campos correctamente para poder enviar el mensaje.");
+    alert(`Gracias, ${nameField.value}. Estaré atendiendo tu mensaje a la brevedad posible.`)
+    clearForm();
+}
+
+function validateForm() {
+    let nameFieldValue = nameField.value;
+    let emailFieldValue = emailField.value;
+    let subjectFieldValue = subjectField.value;
+    let messageFieldValue = messageField.value;
+    return nameFieldValue == "" || emailFieldValue === "" || subjectFieldValue === "" || messageFieldValue === ""
+}
+
+function clearForm() {
+    nameField.value = "";
+    emailField.value = "";
+    subjectField.value = "";
+    messageField.value = "";
+}
+
+contactForm.addEventListener("submit", handleSubmit)
